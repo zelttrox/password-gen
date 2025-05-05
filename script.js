8e0ca4cd1dfab712043bfd7f65547e610f24ddbe
enzo <enzo.granana@ynov.com>
2025-05-05 12:12:30 +0200

    Base html + script

---
+++ script.js
@@ -0,0 +1,16 @@
+const letters = "abcdefghijklmnopqrstuvwxyz"
+const numbers = "0123456789"
+const characters = "-=?!./%_*"
+
+// Generate the password with a given length
+function GeneratePassword() {
+    for (let i = 0; i < letters.length; i++) {
+        const element = letters[i]
+        
+    }
+}
+
+// Display the password on the page
+function PushPassword() {
+
+}
