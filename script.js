3c0f65c85b4e0bedcc18afe5ef4617fe9fb2b14b
enzo <enzo.granana@ynov.com>
2025-05-05 15:12:21 +0200

    Add gen logic + style page

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 const letters = "abcdefghijklmnopqrstuvwxyz"
 const numbers = "0123456789"
-const characters = "-=?!./%_*"
+const specials = "-=?!./%_*"
+
+
+// Return a random number between 0 and x
+function Random(x) {
+    return Math.round(Math.random() * x)
+}
 
 // Generate the password with a given length
-function GeneratePassword() {
-    for (let i = 0; i < letters.length; i++) {
-        const element = letters[i]
-        
+function GeneratePassword(length) {
+    var password = []
+    for (let i = 0; i < length; i++) {
+
+        let chars = Random(2)
+        console.log(chars)
+        switch (chars) {
+            case 0:
+                chars = letters
+                break
+            case 1:
+                chars = numbers
+                break
+            case 2:
+                chars = specials
+        }
+        let output = chars[Random(chars.length)]
+        password.push(output)
     }
+    password = password.join("")
+    PushPassword(password)
 }
 
 // Display the password on the page
-function PushPassword() {
-
+function PushPassword(passwd) {
+    const output_field = document.getElementById("password-output")
+    output_field.innerText = passwd
 }
